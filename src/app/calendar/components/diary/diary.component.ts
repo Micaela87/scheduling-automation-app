ad094c48a5be53257f5bdb6d7e61dea4344140d6
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2023-10-18 18:33:54 +0200

    Add Hours slot per weekday

--- src/app/calendar/components/diary/diary.component.ts
+++ src/app/calendar/components/diary/diary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-diary',
@@ -7,14 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiaryComponent implements OnInit {
 
+  @Input() set events(events: any[]) {
+    console.log(events);
+    if (events?.length) {
+      this.associateEventsToWeekDays(events);
+    }
+  }
+
   weekDays: string[] = ['lun', 'mar', 'mer', 'gio', 'ven', 'sab', 'dom'];
   hours: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
-  dates: number[] = [];
+  dates: { date: number, event?: any }[] = [];
+  month!: number;
+  year!: number;
 
   constructor() { }
 
   ngOnInit(): void {
     const today = new Date();
+    this.month = today.getMonth();
+    this.year = today.getFullYear();
     this.calculateWeekDates(today);
   }
 
@@ -23,8 +34,28 @@ export class DiaryComponent implements OnInit {
     const startingPoint = today.getDate() - weekDay + 1;
 
     for (let i = 0; i < 7; i++) {
-      this.dates.push(startingPoint + i);
+      this.dates.push({ date: startingPoint + i });
     }
+
+  }
+
+  associateEventsToWeekDays(events: any[]) {
+    this.dates = this.dates.map((date: { date: number, event?: any }) => {
+      const formattedDate = `${date.date}/${this.month}/${this.year}`;
+
+      return events.map((event: any) => {
+        const startTime = event.start.dateTime;
+        const eventFormattedDate = `${startTime.getDate()}/${startTime.getMonth()}/${startTime.getFullYear()}`;
+
+        if (formattedDate === eventFormattedDate) {
+          
+          if (date?.event.length) { date.event.push(event) }
+          else { date.event = [event] }
+        }
+
+        return date;
+      })[0];  
+    }).flat();
 
     console.log(this.dates);
   }
