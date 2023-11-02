43b97a0a35b9a009067de07a3bb3f9ab8bfbbca2
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2023-11-02 17:40:49 +0100

    Add Settings main page

--- src/app/settings/components/settings/settings.component.ts
+++ src/app/settings/components/settings/settings.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  weekDays: string[] = ['lun', 'mar', 'mer', 'gio', 'ven', 'sab', 'dom'];
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './components/settings/settings.component';
 import { SettingsRoutingModule } from './routing.module';
+import { WeekDayComponent } from './components/week-day/week-day.component';
 
 
 
 @NgModule({
-  declarations: [SettingsComponent],
+  declarations: [SettingsComponent, WeekDayComponent],
   imports: [
     CommonModule,
     SettingsRoutingModule
